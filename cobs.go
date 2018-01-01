ca6eca6a96a0c4e4f96223c029ee940a39673290
Ivan Boyarkin <boyarkini@eltodo.cz>
2018-01-01 18:51:45 +0100

    Add function for decoding with tests and fix encoding

--- cobs.go
+++ cobs.go
@@ -1,5 +1,7 @@
 package cobs
 
+import "errors"
+
 func Encode(input []byte) ([]byte, error) {
 	// Initialize output array. If input array is empty, then output array will contain {0x01}
 	var output = []byte {0x01}
@@ -12,17 +14,74 @@ func Encode(input []byte) ([]byte, error) {
 
 	// Iterate over input array
 	for i := range input {
-		// TODO: check if delta < 255
+
 		if input[i] == 0x00 {
+
 			output[lastZeroIndex] = delta
 			output = append(output, 0x01)
 			lastZeroIndex = len(output) - 1
 			delta = 1
+
 		} else {
+
+			if delta == 255 {
+
+				output[lastZeroIndex] = delta
+				output = append(output, 0x01)
+				lastZeroIndex = len(output) - 1
+				delta = 1
+
+			}
+
 			output = append(output, input[i])
 			delta++
 		}
 	}
+
 	output[lastZeroIndex] = delta
+	return output, nil
+}
+
+func Decode(input []byte) ([]byte, error){
+
+	if len(input) == 0 {
+		return nil, errors.New("cobs decode: length of input array must be greater than zero")
+	}
+
+	var output = make([]byte, 0)
+	var lastZeroIndex = 0
+
+	for true {
+
+		if lastZeroIndex == len(input){
+			break
+		}
+
+		if input[lastZeroIndex] == 0x00 {
+			return nil, errors.New("cobs decode: zero are not allowed in input array")
+		}
+
+		if int(input[lastZeroIndex]) > (len(input) - int(lastZeroIndex)){
+			return nil, errors.New("cobs decode: zero value refer to outbound of array")
+		}
+
+		var nextZeroIndex = lastZeroIndex + int(input[lastZeroIndex])
+
+		for i:=lastZeroIndex + 1; i < nextZeroIndex; i++ {
+
+			if input[i] == 0x00 {
+				return nil, errors.New("cobs decode: zero value are not allowed in input array")
+			}
+
+			output = append(output, input[i])
+		}
+
+		if nextZeroIndex < len(input) && input[lastZeroIndex] != 0xFF{
+			output = append(output, 0x00)
+		}
+
+		lastZeroIndex = nextZeroIndex
+	}
+
 	return output, nil
 }
--- cobs_test.go
+++ cobs_test.go
@@ -3,6 +3,8 @@ package cobs
 import (
 	"testing"
 	"reflect"
+	"math/rand"
+	"encoding/hex"
 )
 
 type testpair struct {
@@ -10,7 +12,13 @@ type testpair struct {
 	encoded []byte
 }
 
+var array253 = buildArrayWithoutNulls(253)
+var array254 = buildArrayWithoutNulls(254)
+var array255 = append(array254, 0x12)
+var array256 = append([]byte {0x00}, array255...)
+
 var tests = []testpair{
+	// Tests cases for single packet arrays
 	{[]byte{},						[]byte{0x01}},
 	{[]byte{0x00},					[]byte{0x01, 0x01}},
 	{[]byte{0x00, 0x00},				[]byte{0x01, 0x01, 0x01}},
@@ -18,9 +26,14 @@ var tests = []testpair{
 	{[]byte{0x11, 0x22, 0x33, 0x44},	[]byte{0x05, 0x11, 0x22, 0x33, 0x44}},
 	{[]byte{0x11, 0x00, 0x00, 0x00},	[]byte{0x02, 0x11, 0x01, 0x01, 0x01}},
 
+	// Tests cases for length of packet near 254
+	{array253,						append( []byte{0xFE}, 		array253... )},
+	{array254,						append( []byte{0xFF}, 		array254... )},
+	{array255,						append( []byte{0xFF}, 		append( array254, []byte{0x02, 0x12}... )... )},
+	{array256,						append( []byte{0x01, 0xFF}, append( array254, []byte{0x02, 0x12}... )... )},
 }
 
-func TestPreddefinedEncode(t *testing.T){
+func TestPredefinedEncode(t *testing.T){
 	for _, pair := range tests {
 		output, err := Encode(pair.source)
 		if err != nil {
@@ -31,10 +44,100 @@ func TestPreddefinedEncode(t *testing.T){
 		}
 		if !reflect.DeepEqual(output, pair.encoded) {
 			t.Error(
-				"For", pair.source,
-				"got",output,
-				"expected", pair.encoded,
+				"For \n", hex.Dump(pair.source),
+				"got \n", hex.Dump(output),
+				"expected \n", hex.Dump(pair.encoded),
 			)
 		}
 	}
+}
+
+func TestPredefinedDecode(t *testing.T) {
+	for _, pair := range tests {
+		output, err := Decode(pair.encoded)
+		if err != nil {
+			t.Error(
+				"For", hex.Dump(pair.encoded),
+				"got error", err,
+			)
+
+		} else if !reflect.DeepEqual(output, pair.source) {
+			t.Error(
+				"For \n", hex.Dump(pair.encoded),
+				"got \n", hex.Dump(output),
+				"expected \n", hex.Dump(pair.source),
+			)
+		}
+	}
+}
+
+func TestTwoWayEncoding(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		data := buildArrayWithNulls(i*10)
+		encoded, _ := Encode(data)
+		decoded, err := Decode(encoded)
+		if err != nil {
+			t.Error(
+				"For", hex.Dump(encoded),
+				"got error", err,
+			)
+
+		} else if !reflect.DeepEqual(data, decoded) {
+			t.Error(
+				"Source data \n", hex.Dump(data),
+				"encoded to \n", hex.Dump(encoded),
+				"decoded to \n", hex.Dump(decoded),
+			)
+		}
+	}
+}
+
+func TestDecodeNil(t *testing.T) {
+	var data []byte
+	output, err := Decode(data)
+	if err == nil {
+		t.Error("For nil input data expect error, but get: ", hex.Dump(output))
+	}
+}
+
+func TestDecodeWithZeros(t *testing.T) {
+	var data = buildArrayWithNulls(10)
+	encoded, err := Encode(data)
+	if err != nil {
+		t.Error(
+			"For", hex.Dump(encoded),
+			"got error", err,
+		)
+		return
+	}
+
+	// Add zero at random place in encoded slice
+	encoded[ rand.Intn( len( encoded ) - 1 ) + 1 ] = 0x00
+	decoded, err := Decode(encoded)
+	if err == nil {
+			t.Error(
+			"For encoded data with zero \n", hex.Dump(encoded),
+			"get \n", hex.Dump(decoded),
+			"expect error",
+		)
+
+	}
+}
+
+func buildArrayWithoutNulls(length int) []byte {
+	output := make([]byte, length)
+	for i := 0; i < length; i++ {
+		r := rand.Intn(254) + 1
+		output[i] = byte(r)
+	}
+	return output[:]
+}
+
+func buildArrayWithNulls(length int) []byte {
+	output := make([]byte, length)
+	for i := 0; i < length; i++ {
+		r := rand.Intn(255)
+		output[i] = byte(r)
+	}
+	return output[:]
 }
